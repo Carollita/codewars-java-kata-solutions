d5e60ed25f485527b269e7468308fc2d9ce4048b
Carolina <carolsalgadoguadanhini@gmail.com>
2023-02-19 01:11:04 -0300

    add even or odd

---
+++ src/main/java/kyu8/evenorodd/README.md
@@ -0,0 +1,8 @@
+## [Even or Odd](https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java)
+
+![Codewars](https://img.shields.io/badge/Codewars-B1361E?style=for-the-badge&logo=codewars&logoColor=grey)
+![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white)
+
+
+### Instructions
+Create a function that takes an integer as an argument and returns ```"Even"``` for even numbers or ```"Odd"``` for odd numbers.
---
+++ src/test/java/kyu8/evenorodd/EvenOrOddTest.java
@@ -0,0 +1,15 @@
+package kyu8.evenorodd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EvenOrOddTest {
+    @Test
+    public void solution() {
+        EvenOrOdd eoo = new EvenOrOdd();
+        assertEquals("Even", eoo.even_or_odd(6));
+        assertEquals("Odd", eoo.even_or_odd(7));
+        assertEquals("Even", eoo.even_or_odd(0));
+        assertEquals("Odd", eoo.even_or_odd(-1));
+    }
+}
